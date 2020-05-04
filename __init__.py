36fe431cb4be022807544098c2e8cc08faaf09f5
Krzysztof Cyran <krzysztof.cyran@greenmangaming.com>
2020-05-04 18:24:10 +0100

    __init__ changes

--- __init__.py
+++ __init__.py
@@ -109,7 +109,7 @@ def create_app(test_config=None):
         daynum = x.strftime("%d")
         month = x.strftime("%B")
         year = x.strftime("%Y")
-        date = "Today is %s, the %s of %s %s" % day, daynum, month, year
+        date = "Today is {day}, the {daynum} of {month} {year}".format(day, daynum, month, year)
         return render_template('index.html')
 
     @app.route('/log_out', methods=('GET', 'DELETE'))

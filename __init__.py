ef8d1086def1f7e0db7bc4e1f5ccaf77354ad6a1
Krzysztof Cyran <krzysztof.cyran@greenmangaming.com>
2020-05-04 19:38:50 +0100

    Time and date changes

--- __init__.py
+++ __init__.py
@@ -109,7 +109,7 @@ def create_app(test_config=None):
         daynum = x.strftime("%d")
         month = x.strftime("%B")
         year = x.strftime("%Y")
-        date = "Today is {}, the {} of {} {}".format(day, daynum, month, year)
+        date = "Today is {}, the {}'th of {} {}".format(day, daynum, month, year)
         return render_template('index.html', date=date)
 
     @app.route('/log_out', methods=('GET', 'DELETE'))

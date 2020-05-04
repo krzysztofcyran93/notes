d5e0ad179ed1a47e61a488d4b66131a528f72c06
Krzysztof Cyran <krzysztof.cyran@greenmangaming.com>
2020-05-04 18:19:00 +0100

    __init__ changes

--- __init__.py
+++ __init__.py
@@ -11,26 +11,6 @@ from flask import has_request_context, request
 from flask.logging import default_handler
 
 
-
-
-class RequestFormatter(logging.Formatter):
-    def format(self, record):
-        if has_request_context():
-            record.url = request.url
-            record.remote_addr = request.remote_addr
-        else:
-            record.url = None
-            record.remote_addr = None
-
-        return super().format(record)
-
-formatter = RequestFormatter(
-    '[%(asctime)s] %(remote_addr)s requested %(url)s\n'
-    '%(levelname)s in %(module)s: %(message)s'
-)
-default_handler.setFormatter(formatter)
-
-
 def create_app(test_config=None):
     app = Flask(__name__)
     app.config.from_mapping(
